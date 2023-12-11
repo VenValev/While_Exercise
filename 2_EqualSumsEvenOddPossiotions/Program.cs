1d5c52ad6e8330819678fea88c662c4558147a8b
Vencislav <vencislav67@hotmail.com>
2023-12-11 15:51:07 +0200

    2

---
+++ 2_EqualSumsEvenOddPossiotions/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+for (int i = a; i <= b; i++)
+{
+    int sum1 = 0; //chetni
+    int sum2 = 0; //nechetni
+    string cnum = i.ToString();
+    for (int j = 0; j < cnum.Length; j++)
+    {
+        int dig = int.Parse(cnum[j].ToString());
+        if (j % 2 == 0)
+        {
+            sum1 += dig;
+        }
+        else
+        {
+            sum2 += dig;
+        }
+    }
+    if (sum1 == sum2)
+    {
+        Console.Write($"{i} ");
+    }
+
+}
