cc0d0d95ef317f5f9c9f7e72f41ba8c20a23387b
Vencislav <vencislav67@hotmail.com>
2023-12-11 15:42:59 +0200

    Add project files.

---
+++ 1_ReadText/Program.cs
@@ -0,0 +1,12 @@
+﻿int i = 0;
+bool b = true;
+while (i < 9)
+{
+    string s = Console.ReadLine();
+    if (s == "S top") b = false;
+    if (b == true)
+    {
+        Console.WriteLine(s);
+    }
+    i++;
+}
