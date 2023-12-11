24fb4e83e82a0d7a4dacbeb100c0d850c83c72b3
Vencislav <vencislav67@hotmail.com>
2023-12-11 15:53:52 +0200

    5

---
+++ 5_SpecialNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+for (int i = 1111; i <= 9999; i++)
+{
+    int br = 0;
+    for (int j = 0; j < 4; j++)
+    {
+        string s = i.ToString();
+        int c = int.Parse(s[j].ToString());
+        if (c != 0 && n % c == 0)
+        {
+            br++;
+        }
+    }
+    if (br == 4)
+    {
+        Console.Write($"{i} ");
+    }
