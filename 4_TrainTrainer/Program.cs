e5c2d0e745b6d8f72b7341bfa08ecab9b76223cb
Vencislav <vencislav67@hotmail.com>
2023-12-11 15:53:04 +0200

    4

---
+++ 4_TrainTrainer/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+double totalAverage = 0;
+int br = 0;
+while (true)
+{
+    double sumAverage = 0;
+    string s = Console.ReadLine();
+    if (s == "Finish") break;
+    for (int i = 0; i < n; i++)
+    {
+        double a = double.Parse(Console.ReadLine());
+        sumAverage += a;
+    }
+    Console.WriteLine($"{s} - {sumAverage / n:f2}.");
+    br++;
+    totalAverage += sumAverage / n;
+}
+Console.WriteLine($"Student's final assessment is {totalAverage / br:f2}.");
