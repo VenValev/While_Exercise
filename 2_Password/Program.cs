4b1e333f76765d989749cfb92f4bb63ca5004e29
Vencislav <vencislav67@hotmail.com>
2023-12-11 15:44:08 +0200

    2 task done

---
+++ 2_Password/Program.cs
@@ -0,0 +1,8 @@
+﻿string n = Console.ReadLine();
+string p = Console.ReadLine();
+string input = Console.ReadLine();
+while (p != input)
+{
+    input = Console.ReadLine();
+}
+Console.WriteLine($"Welcome {n}!");
