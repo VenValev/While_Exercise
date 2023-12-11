b6bbf19d9551523ab9880371fe74ec1364d7fd1a
Vencislav <vencislav67@hotmail.com>
2023-12-11 15:52:15 +0200

    3

--- 1_NumPyramid/Program.cs
+++ 1_NumPyramid/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = int.Parse(Console.ReadLine());
+int br = 1;
+bool b = false;
+for (int i = 1; i <= a; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        if (br > a)
+        {
+            b = true;
+            break;
+        }
+        Console.Write($"{br} ");
+        br++;
+    }
+    if (b) break;
+    Console.WriteLine();
+}
---
+++ 3_SumPrimeNumPrime/Program.cs
@@ -0,0 +1,35 @@
+﻿int sumSimp = 0;
+int sumNum = 0;
+while (true)
+{
+    string s = Console.ReadLine();
+    if (s == "stop")
+    {
+        break;
+    }
+    bool b = false;
+
+    int num = int.Parse(s);
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            b = true;
+            break;
+        }
+    }
+    if (num < 0)
+    {
+        Console.WriteLine($"Number is negative.");
+    }
+    else if (b)
+    {
+        sumNum += num;
+    }
+    else
+    {
+        sumSimp += num;
+    }
+}
+Console.WriteLine($"Sum of all prime numbers is: {sumSimp}");
+Console.WriteLine($"Sum of all non prime numbers is: {sumNum}");
