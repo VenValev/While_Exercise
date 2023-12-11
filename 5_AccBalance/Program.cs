1eb6b731e6c69036e20c387c7b85211984d27b88
Vencislav <vencislav67@hotmail.com>
2023-12-11 15:46:39 +0200

    5 done

---
+++ 5_AccBalance/Program.cs
@@ -0,0 +1,26 @@
+﻿double sum = 0;
+string s = "";
+while (true)
+{
+    s = Console.ReadLine();
+    if (s == "NoMoreMoney")
+    {
+        Console.WriteLine($"Total: {sum:f2}");
+        break;
+    }
+    else
+    {
+        if (double.Parse(s) < 0)
+        {
+            Console.WriteLine($"Invalid operation!");
+            Console.WriteLine($"Total: {sum:f2}");
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Increase: {double.Parse(s):f2}");
+            sum = sum + double.Parse(s);
+        }
+
+    }
+}
